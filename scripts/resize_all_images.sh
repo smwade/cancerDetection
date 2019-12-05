25c8fd2cfa1b544a3e8669d2637e17f3f821097b
Sean Wade <seanwademail@gmail.com>
2019-12-05 11:48:15 -0800

    resize script

---
+++ scripts/resize_all_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# args: input_dir
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+echo "copying images..."
+cp -r $INPUT_DIR $OUTPUT_DIR
+cd $OUTPUT_DIR
+echo "resizing images..."
+find . -name '*.png' -execdir mogrify -resize $3x$4 {} \;
